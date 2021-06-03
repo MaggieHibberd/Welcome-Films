a627bc51a4fa24fe4832b0708d75027199da49c5
Maggie Hibberd <maggiehibberd@gmail.com>
2021-06-03 18:14:25 +0100

    Search bar performing correctly

--- src/App.js
+++ src/App.js
@@ -5,37 +5,36 @@ import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 
-
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
-  
-  const getMovieRequest = async () => {
-    const url = 'http://www.omdbapi.com/?s=star wars&apikey=87e9b2a4';
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-    const response = await fetch(url);
-    const responseJson = await response.json();
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-      if (responseJson.Search) {
-        setMovies(responseJson.Search);
-      }
-    };
+		const response = await fetch(url);
+		const responseJson = await response.json();
 
-    useEffect(() => {
-      getMovieRequest();
-    }, []);
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
 
-return (
-  <div className='container-fluid movie-app'>
-    <div className='row d-flex align-items-center mt-4 mb-4'>
-    <MovieListHeading heading = 'Movies' />
-    <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-    </div>
-    <div className='row'>
-    <MovieList movies={movies} />
-    </div>
-  </div>
-  );
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList movies={movies} />
+			</div>
+		</div>
+	);
 };
 
 export default App;
